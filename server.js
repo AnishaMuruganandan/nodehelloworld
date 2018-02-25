5d517edce0b382a217acf57f99a6976c3548060f
AnishaMuruganandan <anisha95.m@gmail.com>
2018-02-25 22:59:30 +0530

    changes in path in server.js

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 var express = require("express");
 var app     = express();
+var path = require('path');
 // app.use(express.static(__dirname + '/public/views'));
 //Store all HTML files in view folder.
 // app.use(express.static(__dirname + '/css'));
 //Store all JS and CSS in Scripts folder.
 
-app.get('/',function(req,res){
-  res.sendFile('public/views/index.html');
-  //It will find and locate index.html from View or Scripts
+// app.get('/',function(req,res){
+//   res.sendFile('public/views/index.html');
+//   //It will find and locate index.html from View or Scripts
+// });
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
+
+
 
 // app.get('/about',function(req,res){
 //   res.sendFile('/about.html');
@@ -24,8 +32,3 @@ server.listen(port, function() {
     console.log("App is running on port " + port);
 });
 console.log("Running at Port 3000");
-
-
-
-// Render some console log output
-console.log("Listening on port " + port);
