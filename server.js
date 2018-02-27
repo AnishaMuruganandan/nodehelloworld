f6abf5c27c6041b315ce6e9ce201ec697457682a
AnishaMuruganandan <anisha95.m@gmail.com>
2018-02-28 01:09:13 +0530

    json object

--- server.js
+++ server.js
@@ -2,32 +2,23 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-// Define the port to run on
-//app.set('port', 3000);
+var json = require('express-json');
 
-app.use(express.static(path.join(__dirname, 'public')));
 
-// Listen for requests
-// var server = app.listen(app.get('port'), function() {
-//   var port = server.address().port;
-//   console.log('Magic happens on port ' + port);
-// });
-var port = process.env.PORT || 3000;
-app.listen(port, "0.0.0.0", function() {
-console.log("Listening on Port" +port);
+//<--To Display static html page -->
+//app.use(express.static(path.join(__dirname, 'public')));
+//app.use(json());
+//<!--To Display json object -->
+//app.use method accepts any http request
+app.use(function (req, res) {
+  console.log('Request Type:', req.method);
+  res.json({
+      helloWorld: 'Hello World!'
+  });
 });
 
 
-
-// var express = require("express");
-// var app     = express();
-// var path = require('path');
-//
-// app.get('/', function(req, res) {
-//     res.sendFile(path.join(__dirname + '/index.html'));
-// });
-//
-// var port = process.env.PORT || 8000;
-// app.listen(port, function() {
-//     console.log("App is running on port " + port);
-// });
+var port = process.env.PORT || 4000;
+app.listen(port, "0.0.0.0", function() {
+console.log("Listening on Port" +port);
+});
