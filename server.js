b2b3e988902df0568ba7005deeedb5e6d47374e4
AnishaMuruganandan <anisha95.m@gmail.com>
2018-03-01 02:15:48 +0530

    using mlab

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-var json = require('express-json');
+//<!--To include json-->
+//var json = require('express-json');
 
 
 //<--To Display static html page -->
@@ -18,19 +19,44 @@ var json = require('express-json');
 // });
 
 //<!--Query parameteer value fetching-->
-app.use(function (req, res) {
+// app.use(function (req, res) {
   //pareInt is to convert the value to pareInt
   //req.param(key passed in url) will fetch value from url
-   var value1 = parseInt(req.param('a'));
-   var value2 = parseInt(req.param('b'));
+   // var value1 = parseInt(req.param('a'));
+   // var value2 = parseInt(req.param('b'));
    //res.json method to return json object
-  res.json({
-      value: value1+value2
+//   res.json({
+//       value: value1+value2
+//   });
+// });
+const MongoClient = require('mongodb').MongoClient
+
+app.use(function (req, res) {
+//<!--To write into collection into mlab db-->
+  //  var myobj = { name: "Company Inc", address: "Highway 37" };
+  // db.collection('quotes').save(myobj, (err, result) => {
+  //   if (err) return console.log(err)
+  //
+  //   console.log('saved to database')
+  //   res.redirect('/')
+  //<!--end of writing collection-->
+  // var cursor = db.collection('quotes').find()
+  //<!--To read the collection from mlab-->
+  db.collection('quotes').find().toArray(function(err, results) {
+  console.log(results)
+  //<!--end of read>
+  // send HTML file populated with quotes here
+})
+//  })
+})
+var db
+  var port = process.env.PORT || 4000;
+
+MongoClient.connect('mongodb://root:root@ds151908.mlab.com:51908/details', (err, client) => {
+  if (err) return console.log(err)
+  db = client.db('details') // whatever your database name is
+
+  app.listen(port, "0.0.0.0", function() {
+  console.log("Listening on Port" +port);
   });
-});
-
-
-var port = process.env.PORT || 4000;
-app.listen(port, "0.0.0.0", function() {
-console.log("Listening on Port" +port);
-});
+})
