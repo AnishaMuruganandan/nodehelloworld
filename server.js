da13b2554c6ebfe16760722a526112885651d903
AnishaMuruganandan <anisha95.m@gmail.com>
2018-03-04 01:21:45 +0530

    Simple Web App

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 //<!--To include json-->
 var json = require('express-json');
 
-
+var bodyParser = require('body-parser');
 //<--To Display static html page -->
 //app.use(express.static(path.join(__dirname, 'public')));
 //app.use(json());
@@ -29,33 +29,68 @@ var json = require('express-json');
 //       value: value1+value2
 //   });
 // });
+
+//<!-- Mongodb to store url values [query paramaters]-->
+// const MongoClient = require('mongodb').MongoClient
+//
+// app.use(function (req, res) {
+// //<!--To write into collection into mlab db-->
+// var value1 = req.param('a');
+//  var value2 = req.param('b');
+//
+//    var myobj = { name: value1, address: value2 };
+//   db.collection('info').save(myobj, (err, result) => {
+//    if (err) return console.log(err)
+//
+//   console.log('saved to database')
+//    })
+//   //<!--end of writing collection-->
+//   //<!--To read the collection from mlab-->
+//   db.collection('info').find().toArray(function(err, result) {
+//   //console.log(results)
+//   res.json({
+// result
+//     });
+//   //<!--end of read>
+//   // send HTML file populated with quotes here
+// })
+//
+// })
+//<!--end of uery paramaters-->
+
+//<!--Simple web app to store value from forms-->
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 const MongoClient = require('mongodb').MongoClient
+//app.use(bodyParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.post('/endpoint', function(req, res){
+	var obj = {};
+	console.log('body: ' + JSON.stringify(req.body));
+	
 
-app.use(function (req, res) {
-//<!--To write into collection into mlab db-->
-var value1 = req.param('a');
- var value2 = req.param('b');
+  var str2json = require('string-to-json');
 
-   var myobj = { name: value1, address: value2 };
-  db.collection('info').save(myobj, (err, result) => {
+    //  var output = str2json.convert({"abc.def.g":2});
+
+
+    var myobj = str2json.convert(req.body);
+    console.log('obj'+myobj);
+//{ name: value1, address: value2 };
+    db.collection('tabl').save(myobj, (err, result) => {
    if (err) return console.log(err)
 
-  console.log('saved to database')
-   })
-  //   res.redirect('/')
-  //<!--end of writing collection-->
-  // var cursor = db.collection('quotes').find()
-  //<!--To read the collection from mlab-->
-  db.collection('info').find().toArray(function(err, result) {
-  //console.log(results)
-  res.json({
-result
-    });
-  //<!--end of read>
-  // send HTML file populated with quotes here
+ console.log('saved to database')
+    })
+    db.collection('tabl').find().toArray(function(err, result) {
+    console.log(result)
+  res.send(result);
 })
+});
 
-})
 var db
   var port = process.env.PORT || 4000;
 
